f614fb8c0487448f0248ce661e3ab689f4a63a6f
itzgitJitesh <jitesh.thehustlercollective@gmail.com>
2024-06-21 18:28:21 +0530

    renamed:    .htaccess -> thc_test/.htaccess 	renamed:    404.php -> thc_test/404.php 	renamed:    about.php -> thc_test/about.php 	renamed:    contact.php -> thc_test/contact.php 	renamed:    css/style.css -> thc_test/css/style.css 	renamed:    function.php -> thc_test/function.php 	renamed:    images/samp.jpg -> thc_test/images/samp.jpg 	renamed:    images/sampl.jpg -> thc_test/images/sampl.jpg 	renamed:    images/sample.jpg -> thc_test/images/sample.jpg 	renamed:    index.php -> thc_test/index.php 	renamed:    js/script.js -> thc_test/js/script.js 	renamed:    service.php -> thc_test/service.php 	renamed:    views/about.view.php -> thc_test/views/about.view.php 	new file:   thc_test/views/config.php 	renamed:    views/form.php -> thc_test/views/form.php 	renamed:    views/home/about.php -> thc_test/views/home/about.php 	renamed:    views/home/banner-slider.php -> thc_test/views/home/banner-slider.php 	renamed:    views/home/gallery.php -> thc_test/views/home/gallery.php 	renamed:    views/home/service.php -> thc_test/views/home/service.php 	renamed:    views/partials/footer.php -> thc_test/views/partials/footer.php 	renamed:    views/partials/head.php -> thc_test/views/partials/head.php 	renamed:    views/partials/header.php -> thc_test/views/partials/header.php 	new file:   thc_test/views/userinfo.php 	deleted:    userinfo.php

---
+++ thc_test/views/config.php
@@ -0,0 +1,15 @@
+<?php
+// Database configuration
+// $db_host = 'localhost';
+// $db_user = 'root';
+// $db_pass = '';
+// $db_name = 'my_database';
+
+// // Create connection
+// $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+?>
